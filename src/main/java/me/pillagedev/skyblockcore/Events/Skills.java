8dcf8c06c3ae96926e886f7c5bd4fabdca88122d
PillageDev <pillageyt@gmail.com>
2023-05-15 21:02:45 -0400

    Added equation to determine xp requirement & added mining skill

--- src/main/java/me/pillagedev/skyblockcore/Events/Skills.java
+++ src/main/java/me/pillagedev/skyblockcore/Events/Skills.java
@@ -9,6 +9,9 @@ import org.bukkit.event.Listener;
 import com.google.common.collect.Table;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Skills implements Listener {
     Table<String, String, Integer> skillLevel = HashBasedTable.create();
     Table<String, String, Float> skillExp = HashBasedTable.create();
@@ -42,11 +45,67 @@ public class Skills implements Listener {
                 skillExp.put(player.getName(), "farming", newExp);
             }
         }
-        float xp = 50 * farmingLevel(player) * (farmingLevel(player) + 1);
+        //y ≈ 3.4608x^3 - 25.2433x^2 + 68.5532x + 7.7537
+        float calcXP = (float) ((Math.pow((3.4608 * (farmingLevel(player) + 1)), 3)) - (Math.pow((25.2433 * (farmingLevel(player) + 1)), 2)) + (68.5532 * (farmingLevel(player) + 1)) + 7.7537);
+        int xp = Math.round(calcXP);
         while (currentExp >= xp) {
             currentExp -= xp;
             skillLevel.put(player.getName(), "farming", farmingLevel(player) + 1);
-            xp = 50 * farmingLevel(player) * (farmingLevel(player) + 1);
+            //TODO: Add farming level up message
+        }
+    }
+
+    public int miningLevel(Player player) {
+        return skillLevel.get(player.getName(), "mining");
+    }
+
+    @EventHandler
+    public void miningSkill(BlockBreakEvent event) {
+        float currentExp = skillExp.get(event.getPlayer().getName(), "mining");
+        Player player = event.getPlayer();
+        Material material = event.getBlock().getType();
+        if (material == Material.COBBLESTONE || material == Material.STONE) {
+            float newExp = currentExp + 1; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.COAL_ORE || material == Material.IRON_ORE) {
+            float newExp = currentExp + 5; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.GOLD_ORE) {
+            float newExp = currentExp + 6; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.LAPIS_ORE || material == Material.REDSTONE_ORE) {
+            float newExp = currentExp + 7; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.EMERALD_ORE) {
+            float newExp = currentExp + 9; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.DIAMOND_ORE) {
+            float newExp = currentExp + 10; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.DIAMOND_BLOCK) {
+            float newExp = currentExp + 15; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.OBSIDIAN) {
+            float newExp = currentExp + 20; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.END_STONE) {
+            float newExp = currentExp + 3; //TODO: Incorporate mining wisdom
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.NETHERRACK) {
+            float newExp = (float) (currentExp + 0.5);
+            skillExp.put(player.getName(), "mining", newExp);
+        } else if (material == Material.SAND) {
+            float newExp = currentExp + 3;
+            skillExp.put(player.getName(), "mining", newExp);
+        }
+
+        //y ≈ 3.4608x^3 - 25.2433x^2 + 68.5532x + 7.7537
+        float calcXP = (float) ((Math.pow((3.4608 * (miningLevel(player) + 1)), 3)) - (Math.pow((25.2433 * (miningLevel(player) + 1)), 2)) + (68.5532 * (miningLevel(player) + 1)) + 7.7537);
+        int xp = Math.round(calcXP);
+        while (currentExp >= xp) {
+            currentExp -= xp;
+            skillLevel.put(player.getName(), "mining", miningLevel(player) + 1);
+            //TODO: Add mining level up message
         }
     }
 
